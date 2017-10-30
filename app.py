820667a81c6e0903d3453769ebe4b94f7a02dbe4
Vishnu ks <ksvishnu56@gmail.com>
2017-10-30 21:24:55 +0530

    refresh token api implemented

--- app.py
+++ app.py
@@ -1,86 +1,102 @@
-import json
-from flask_cors import CORS
-from flask import Flask,request,abort,jsonify
-from login_register import signup,login,validate_user,profile_details, decode_refresh_token
 import ast
+import json
 
-app = Flask(__name__)
+from flask import Flask, request, abort, jsonify
+from flask_cors import CORS
+
+from login_register import signup, login, validate_user, logout, profile_details, decode_refresh_token
 
 app = Flask(__name__)
 app.secret_key = 'N@\xc4\x1bC\xb7\xd0\xbc\xb4\tA\xd9\xcb\x13I?\x92\x104\x1b\xaa\xdc}\xca'
 CORS(app)
+
+
 @app.route('/')
 def hello_world():
     return 'Hi!'
 
-@app.route('/refresh/',methods=['POST'])
+
+@app.route('/refresh/', methods=['POST'])
 def get_refresh_response():
-   if not request.json or 'refresh_token' not in request.json:
-      print request.json
-      abort(400)
-   data = {}
-   refresh_token = request.json['refresh_token']
-   data['response'] = decode_refresh_token(refresh_token)
-   return jsonify(data)
+    if not request.json or 'refresh_token' not in request.json:
+        print request.json
+        abort(400)
+    data = {}
+    refresh_token = request.json['refresh_token']
+    data['response'] = decode_refresh_token(refresh_token)
+    return jsonify(data)
 
-@app.route('/signup/',methods=['POST'])
+
+@app.route('/logout/', methods=['POST'])
+def get_logout_response():
+    if not request.json or 'refresh_token' not in request.json:
+        abort(400)
+    response = logout(request.json['refresh_token'])
+    return jsonify(response)
+
+
+@app.route('/signup/', methods=['POST'])
 def get_signup_response():
-   k=('username','password','dob','name','lat','lon','email','language','tob','gender','pob')
-   if not request.json or not all(i in request.json for i in k):
-     abort(400)
-   data={}
-   query={}
-   print request.json
-   query["username"]=request.json['username']
-   query["password"]=request.json['password']
-   query["dob"]=request.json['dob']
-   query["name"]=request.json['name']
-   query["lat"]=request.json['lat']
-   query["lon"]=request.json['lon']
-   query["email"]=request.json['email']
-   query["language"]=request.json['language']
-   query["tob"]=request.json['tob']
-   query["gender"]=request.json['gender']
-   query["pob"]=request.json['pob']
-   data['response']=signup(query)
-   json_data=json.dumps(data)
-   return(json_data),201
+    k = ('username', 'password', 'dob', 'name', 'lat', 'lon', 'email', 'language', 'tob', 'gender', 'pob')
+    if not request.json or not all(i in request.json for i in k):
+        abort(400)
+    data = {}
+    query = {}
+    print request.json
+    query["username"] = request.json['username']
+    query["password"] = request.json['password']
+    query["dob"] = request.json['dob']
+    query["name"] = request.json['name']
+    query["lat"] = request.json['lat']
+    query["lon"] = request.json['lon']
+    query["email"] = request.json['email']
+    query["language"] = request.json['language']
+    query["tob"] = request.json['tob']
+    query["gender"] = request.json['gender']
+    query["pob"] = request.json['pob']
+    data['response'] = signup(query)
+    json_data = json.dumps(data)
+    return (json_data), 201
 
-@app.route('/login/',methods=['POST'])
+
+@app.route('/login/', methods=['POST'])
 def get_login_response():
-   if not request.json or 'email' not in request.json:
-     abort(400)
-   data={}
-   query={}
-   query['email']=request.json['email']
-   query['password']=request.json['password']
-   data['response']=login(query)
-   print data['response']
-   if data['response']=='Invalid':
-      return jsonify(data)
-   del data['response']['data_profile']['_id']
-   json_data=jsonify(data)
-   return(json_data),201
+    if not request.json or 'email' not in request.json:
+        abort(400)
+    data = {}
+    query = {}
+    query['email'] = request.json['email']
+    query['password'] = request.json['password']
+    data['response'] = login(query)
+    print data['response']
+    if data['response'] == 'Invalid':
+        return jsonify(data)
+    del data['response']['data_profile']['_id']
+    json_data = jsonify(data)
+    return (json_data), 201
+
 
 @app.route("/validate_user/<string:query>")
 def get_validate_response(query):
-   data=ast.literal_eval(query)
-   dicte={}
-   dicte["response"]=validate_user(data)
-   json_data=jsonify(dicte)
-   return(json_data)
+    data = ast.literal_eval(query)
+    dicte = {}
+    dicte["response"] = validate_user(data)
+    json_data = jsonify(dicte)
+    return (json_data)
 
-@app.route('/profile/',methods=['POST'])
+
+@app.route('/profile/', methods=['POST'])
 def profile():
-   if not request.json or 'access_token' not in request.json:
-      print request.json
-      abort(400)
-   data = {}
-   query = {}
-   query['access_token'] = request.json['access_token']
-   data['response'] = profile_details(query)
-   json_data=jsonify(data)
-   return json_data
+    if not request.json or 'access_token' not in request.json:
+        print request.json
+        abort(400)
+    data = {}
+    query = {}
+    query['access_token'] = request.json['access_token']
+    data['response'] = profile_details(query)
+    json_data = jsonify(data)
+    return json_data
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
--- login_register.py
+++ login_register.py
@@ -1,23 +1,10 @@
-from pymongo import MongoClient
 import datetime
+
+import MySQLdb
 import jwt
+from pymongo import MongoClient
 
-SECRET_KEY='N@\xc4\x1bC\xb7\xd0\xbc\xb4\tA\xd9\xcb\x13I?\x92\x104\x1b\xaa\xdc}\xca'
-
-def signup(data):
-  client=MongoClient("127.0.0.1",27017,maxPoolSize=None)
-  db=client.kolam
-  dicte=data
-  existing_doc=db.login.find_one({"email":dicte["email"]})
-  client.close()
-  if not existing_doc:
-    result=db.login.insert(dicte)
-    if result:
-      return "Inserted successfully"
-    else:
-      return "Try later"
-  else:
-     return "data already present"
+SECRET_KEY = 'N@\xc4\x1bC\xb7\xd0\xbc\xb4\tA\xd9\xcb\x13I?\x92\x104\x1b\xaa\xdc}\xca'
 
 def refresh_id(user_id):
     """
@@ -37,19 +24,80 @@ def refresh_id(user_id):
     except Exception as e:
         return e
 
-def decode_refresh_token(auth_token):
+
+def signup(data):
+    client = MongoClient("127.0.0.1", 27017, maxPoolSize=None)
+    mongodb = client.kolam
+    dicte = data
+    existing_doc = mongodb.login.find_one({"email": dicte["email"]})
+    if not existing_doc:
+        result = 0
+        mysql = MySQLdb.connect("localhost", "root", "vishnuks", "refresh_tokens")
+        cursor = mysql.cursor()
+        try:
+            cursor.execute("insert into refresh_ids(user,token,is_delete) values(%s,%s,%s)",
+                           (dicte['email'], refresh_id(dicte['email']), 0))
+            mysql.commit()
+            mysql.close()
+            result = mongodb.login.insert(dicte)
+            client.close()
+        except Exception as e:
+            mysql.rollback()
+            mysql.close()
+            print "Unsuccessful  while adding refresh tokens :", e, len(refresh_id(dicte['email']))
+        finally:
+            if result:
+                return "Inserted successfully"
+            else:
+                return "Try later"
+    else:
+        return "data already present"
+
+
+def logout(token):
+    user = decode_refresh_token(token)
+    user = user['sub']
+    mysql = MySQLdb.connect("localhost", "root", "vishnuks", "refresh_tokens")
+    cursor = mysql.cursor()
+    cmd = "update refresh_ids set is_delete=TRUE where user='" + user + "'"
+    try:
+        cursor.execute(cmd)
+        mysql.commit()
+        mysql.close()
+        return ({'status': 'True', 'response': 'logged out'})
+    except Exception as e:
+        mysql.rollback()
+        mysql.close()
+        print "Here is an error: ", e
+        return ({'status': 'False', 'response': 'Something went wrong'})
+
+
+def decode_refresh_token(refresh_token):
     """
-    Decodes the auth token
-    :param auth_token:
+    Decodes the refresh token and generates a access token
+    :param refresh_token:
     :return: integer|string
     """
+    client = MongoClient("127.0.0.1", 27017, maxPoolSize=None)
+    db = client.kolam
     try:
-        payload = jwt.decode(auth_token, SECRET_KEY)
-        return ({'sub':payload['sub']})
+        payload = jwt.decode(refresh_token, SECRET_KEY)
+        user = payload['sub']
+        mysql = MySQLdb.connect("localhost", "root", "vishnuks", "refresh_tokens")
+        cursor = mysql.cursor()
+        result = cursor.execute("select user from refresh_ids where user='" + user + "' and is_delete=1")
+        print result
+        if result:
+            return ({'Message': "You are already logged out. Please login again with your credentials to continue",
+                     'response': 'False'})
+        else:
+            access_token = encode_auth_token(user)
+            return ({'Message': "Successful", "access_token": access_token, "status": 'True'})
     except jwt.ExpiredSignatureError:
-        return ({'status':'failed','msg':'Signature expired. Please log in again.'})
+        return ({'status': 'failed', 'msg': 'Signature expired. Please log in again.'})
     except jwt.InvalidTokenError:
-        return ({'status':'failed','msg':'Invalid token. Please log in again.'})
+        return ({'status': 'failed', 'msg': 'Invalid token. Please log in again.'})
+
 
 def encode_auth_token(user_id):
     """
@@ -70,6 +118,7 @@ def encode_auth_token(user_id):
     except Exception as e:
         return e
 
+
 def decode_auth_token(auth_token):
     """
     Decodes the auth token
@@ -78,58 +127,62 @@ def decode_auth_token(auth_token):
     """
     try:
         payload = jwt.decode(auth_token, SECRET_KEY)
-        return ({'sub':payload['sub']})
+        return ({'sub': payload['sub']})
     except jwt.ExpiredSignatureError:
-        return ({'status':'failed','msg':'Signature expired. Please log in again.'})
+        return ({'status': 'failed', 'msg': 'Signature expired. Please log in again.'})
     except jwt.InvalidTokenError:
-        return ({'status':'failed','msg':'Invalid token. Please log in again.'})
+        return ({'status': 'failed', 'msg': 'Invalid token. Please log in again.'})
 
 
 def login(data):
-  client=MongoClient("127.0.0.1",27017,maxPoolSize=None)
-  db=client.kolam
-  dicte=data
-  keys=dicte.keys()
-  keys=sorted(keys)
-  k=["email","password"]
-  if(keys==k):
-    existing_doc=db.login.find_one(dicte)
-    client.close()
-    if existing_doc:
-      return({'status':"Succesfull",'access_token':encode_auth_token(dicte['email']),'data_profile':existing_doc,'refresh_id':refresh_id(dicte['email'])})
+    client = MongoClient("127.0.0.1", 27017, maxPoolSize=None)
+    db = client.kolam
+    dicte = data
+    keys = dicte.keys()
+    keys = sorted(keys)
+    k = ["email", "password"]
+    if (keys == k):
+        existing_doc = db.login.find_one(dicte)
+        client.close()
+        if existing_doc:
+            return (
+                {'status': "Succesfull", 'access_token': encode_auth_token(dicte['email']),
+                 'data_profile': existing_doc,
+                 'refresh_id': refresh_id(dicte['email'])})
+        else:
+            return ("Invalid")
     else:
-      return("Invalid")
-  else:
-    return("Invalid")
+        return ("Invalid")
+
 
 def validate_user(data):
-  client=MongoClient("127.0.0.1",27017,maxPoolSize=None)
-  db=client.kolam
-  d=db.login.find_one(data)
-  client.close()
-  response={}
-  if d:
-    response["status"]="valid"
-    return response
-  else:
-    return "invalid"
+    client = MongoClient("127.0.0.1", 27017, maxPoolSize=None)
+    db = client.kolam
+    d = db.login.find_one(data)
+    client.close()
+    response = {}
+    if d:
+        response["status"] = "valid"
+        return response
+    else:
+        return "invalid"
+
 
 def profile_details(query):
-  client=MongoClient("127.0.0.1",27017,maxPoolSize=None)
-  db=client.kolam
-  access_token=query['access_token']
-  dicte=decode_auth_token(access_token)
-  print dicte
-  if 'status' in dicte:
-    return dicte
-  else:
-    data={}
-    data['username']=dicte['sub']
-    existing_doc = db.login.find_one(data,{'_id':False})
-    client.close()
-    if existing_doc:
-      existing_doc['new_token']=encode_auth_token(data['username'])
-      return existing_doc
+    client = MongoClient("127.0.0.1", 27017, maxPoolSize=None)
+    db = client.kolam
+    access_token = query['access_token']
+    dicte = decode_auth_token(access_token)
+    print dicte
+    if 'status' in dicte:
+        return dicte
     else:
-      return 'Data not found for this user'
-  
+        data = {}
+        data['username'] = dicte['sub']
+        existing_doc = db.login.find_one(data, {'_id': False})
+        client.close()
+        if existing_doc:
+            existing_doc['new_token'] = encode_auth_token(data['username'])
+            return existing_doc
+        else:
+            return 'Data not found for this user'
